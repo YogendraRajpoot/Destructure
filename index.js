381fedf0208add7681a977b3d0d1c9e6995dc168
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-02-01 12:47:21 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // destructure the name and password from
 // {  name: 'masai', password: 'admin@123', hostname:'masaischool', client: 'chrome' }
+console.log(`First Problem :- 1`);
 let data = {
   name: "masai",
   password: "admin@123",
@@ -13,9 +14,9 @@ let data = {
 
 let { name, password } = data;
 console.log(`Name : ${name}\nPassword : ${password} `);
-
+console.log(`***********************`);
 /***************************************************************************************************************/
-
+console.log(`First Problem :- 2`);
 // destructure the pincode and rename to pin
 // {  name: 'masai', address: {  pincode: 560095 }  }
 data = { 
@@ -26,35 +27,37 @@ data = {
 };
 let {address:{pincode : pin}} = data
 console.log(`Pin : ${pin} `);
+console.log(`***********************`);
 
 
 /***************************************************************************************************************/
 // destructure the first value and second value of the array
 // [ 100, 200, 500, 600 ]
-
+console.log(`First Problem :- 3`);
 var [a,b,c,d] = [ 100, 200, 500, 600 ]
 console.log(a,b);
-
+console.log(`***********************`);
 
 
 /***************************************************************************************************************/
 
 // destructure to get the value 50 from this array
 // [ 100, [  50, 60, 70  ]   ]
-
+console.log(`First Problem :- 4`);
 var [a,[b,c,d]] = [ 100, [  50, 60, 70  ]   ]
 console.log(b);
-
+console.log(`***********************`);
 /***************************************************************************************************************/
 // destructure to get the value 2,3 into a new array
 // [  1, {
 //      arr: [ 1, 2, 3 ]
 //    }
 //  ]
+console.log(`First Problem :- 5`);
 
 var [a,{arr:[b,c,d]}] = [  1, {arr: [ 1, 2, 3 ] } ] 
 console.log(c,d);
-
+console.log(`***********************`);
 
 
 
